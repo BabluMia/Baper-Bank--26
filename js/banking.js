23e49bfe018802290d5448c8b81ab4d6f6078b20
Bablu Mia <bablumiah2000@gmail.com>
2022-02-10 23:29:31 +0600

    fainal add

--- js/banking.js
+++ js/banking.js
@@ -1,13 +1,13 @@
 // handle deposit 
-document.getElementById('deposit-button').addEventListener('click', function(){
+document.getElementById('deposit-button').addEventListener('click', function () {
     // deposit 
     let depositInput = document.getElementById('deposit-input');
     let newdepositAmount = depositInput.value;
 
     // console.log(depositAmount);
     let depositTotal = document.getElementById('deposit-total');
-    let previousDepositAmount = depositTotal.innerText; 
-    let newDepositTotal =parseFloat(previousDepositAmount)  + parseFloat(newdepositAmount) ;
+    let previousDepositAmount = depositTotal.innerText;
+    let newDepositTotal = parseFloat(previousDepositAmount) + parseFloat(newdepositAmount);
     depositTotal.innerText = newDepositTotal;
 
     // update balance
@@ -18,4 +18,27 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     balanceTotal.innerText = newBalanceTotal;
     // value
     depositInput.value = '';
+})
+document.getElementById('withdrow-button').addEventListener('click', function () {
+    let withdrowInput = document.getElementById('withdrow-input');
+    let withdrowAmountText = withdrowInput.value;
+    let newWithdrowAmount = parseFloat(withdrowAmountText)
+    // console.log(newWithdrowAmount)
+    //set withdrow total
+    let withdrowTotal  =document.getElementById('withdrow-total');
+    let previousWithdrowText = withdrowTotal.innerText;
+    let previousWithdrowTotal = parseFloat(previousWithdrowText);
+    let newWithdrowTotal = previousWithdrowTotal + newWithdrowAmount;
+    withdrowTotal.innerText = newWithdrowTotal;
+    // update balance
+    let balanceTotal = document.getElementById('balance-total');
+    let peviousBalanceText = balanceTotal.innerText;
+    let peviousBalanceTotal = parseFloat(peviousBalanceText);
+    let newBalanceTotal = peviousBalanceTotal - newWithdrowAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+
+    // clear
+    withdrowInput.value = '';
 })

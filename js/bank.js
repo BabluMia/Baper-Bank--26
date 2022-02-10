1e95f522977134298566bf9ef39ecdb7689cc0b9
Bablu Mia <bablumiah2000@gmail.com>
2022-02-10 20:43:45 +0600

    site added

--- js/bank.js
+++ js/bank.js
@@ -1,12 +1,14 @@
 document.getElementById('login-submit').addEventListener('click',function(){
     // get user email
     let emailField = document.getElementById('input-email');
-    let userEmail = emailField.Value;
+    let userEmail = emailField.value;
     // get user pass
     let passField = document.getElementById('input-pass')
     let userPass = passField.value ;
     // condition
-    if(userEmail == 'abc' && userPass == 'secret'){
-        console.log('Valid User!!');
+    if(userEmail == 'abc@gmail.com' && userPass == 'abc'){
+        window.location.href = 'banking.html'
+    } else{
+       alert('Invalid Input')
     }
 })
